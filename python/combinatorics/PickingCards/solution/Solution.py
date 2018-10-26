b1fc73c46cfb743c80c80582962afff17d8eb1db
chrislucas <christoffer.luccas@gmail.com>
2018-10-26 17:52:32 -0300

    estudo

--- python/combinatorics/PickingCards/solution/Solution.py
+++ python/combinatorics/PickingCards/solution/Solution.py
@@ -13,6 +13,7 @@ def run():
         total = int(input().rstrip())
         counter_cards = [int(x) for x in input().rstrip().split(" ")]
         _map = [0] * total
+        _acc = [0] * total
         flag = True
         for i in range(0, total):
             if counter_cards[i] < total:
@@ -21,13 +22,19 @@ def run():
                 flag = False
                 break
 
+        _acc[0] = _map[0]
+        for i in range(1, len(_map)):
+            _acc[i] = _acc[i-1] + _map[i]
+
         acc = 0
         if flag:
             acc = 1
-            for v in counter_cards:
-                q_cards = _map[v]
-                acc = mult_mod(acc, q_cards, 1000000007)
-
+            for i in range(0, total):
+                val = _acc[i] - i
+                if val <= 0:
+                    acc = 1
+                    break
+                acc = mult_mod(acc, val, 1000000007)
 
         print(acc)
 
